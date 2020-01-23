8388661c19457bb2978331704da8cf27ab42162b
Raphaël Lavoie <rapolysec@hotmail.com>
2020-01-23 14:27:14 -0500

    Progrès

---
+++ Assets/Scripts/KinematicTower/TowerArmJoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerArmJoint : MonoBehaviour
+{
+    public Vector3 Axis;
+    public Vector3 StartOffset;
+
+    void Awake()
+    {
+        StartOffset = transform.localPosition;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
