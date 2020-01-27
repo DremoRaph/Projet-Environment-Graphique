737901c2945f7a0f119d1921670c3135ab1bc08c
TheDude95100 <58417329+TheDude95100@users.noreply.github.com>
2020-01-27 14:26:53 -0500

    Ajout de l'ennemi
    Deplacement

---
+++ Assets/Scripts/Controllers/EnenyController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnenyController : MonoBehaviour
+{
+
+    Transform targetPlayer;
+    public float lookRadiusPlayer = 10f;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        targetPlayer = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(targetPlayer.position, transform.position);
+
+        if (distance <= lookRadiusPlayer)
+        {
+            agent.SetDestination(targetPlayer.position);
+            Debug.Log(targetPlayer);
+        }
+        if (distance <= agent.stoppingDistance)
+        {
+            FaceTarget();
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadiusPlayer);
+    }
+    void FaceTarget()
+    {
+        Vector3 direction = (targetPlayer.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp( transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+}
