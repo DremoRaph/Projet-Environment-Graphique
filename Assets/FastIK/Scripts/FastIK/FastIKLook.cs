ed76e6b5b26c70eff4a929348936ded6d8eee133
Raphaël Lavoie <rapolysec@hotmail.com>
2020-01-29 17:14:45 -0500

    Nettoyage, import d'asset(s) et première animation procédurale fonctionnelle

---
+++ Assets/FastIK/Scripts/FastIK/FastIKLook.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+namespace DitzelGames.FastIK
+{
+    public class FastIKLook : MonoBehaviour
+    {
+        /// <summary>
+        /// Look at target
+        /// </summary>
+        public Transform Target;
+
+        public enum FreezedAxisEnum {NONE,X,Y,Z};
+        public FreezedAxisEnum FreezedAxis = FreezedAxisEnum.NONE;
+
+        /// <summary>
+        /// Initial direction
+        /// </summary>
+        protected Vector3 StartDirection;
+
+        /// <summary>
+        /// Initial Rotation
+        /// </summary>
+        protected Quaternion StartRotation;
+
+        void Awake()
+        {
+            if (Target == null)
+                return;
+
+            StartDirection = Target.position - transform.position;
+            StartRotation = transform.rotation;
+        }
+
+        void Update()
+        {
+            if (Target == null)
+                return;
+            Vector3 t_Pointer = Target.position;
+            if (FreezedAxis != FreezedAxisEnum.NONE)
+            {
+                if(FreezedAxis == FreezedAxisEnum.X)
+                {
+                    transform.LookAt(new Vector3(transform.position.x, Target.position.y, Target.position.z));
+                    return;
+                } else if (FreezedAxis == FreezedAxisEnum.Y)
+                {
+                    transform.LookAt(new Vector3(Target.position.x, transform.position.y , Target.position.z));
+                    return;
+                } else if (FreezedAxis == FreezedAxisEnum.Z)
+                {
+                    transform.LookAt(new Vector3(Target.position.x, Target.position.y, transform.position.z));
+                    return;
+                }
+            }
+            
+            transform.rotation = Quaternion.FromToRotation(StartDirection, Target.position - transform.position) * StartRotation;
+            
+            
+
+        }
+    }
+}
---
+++ Assets/Scripts/Animations/BirdAnimation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdAnimation : MonoBehaviour
+{
+    public GameObject WingTarget;
+    [Range(0.0f, 180.0f)]
+    public float WingsAngle = 90f;
+    public float FlapHeight = 0.44f;
+    public float TimeForCycle = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (WingTarget)
+        {
+            StartCoroutine(WingFlap());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Find("Body").localRotation = Quaternion.Euler(new Vector3((-WingsAngle + 90) / 1.5f, 0, 0));
+        transform.Find("Wings").localRotation = Quaternion.Euler(new Vector3(-WingsAngle + 90, 0, 0));
+    }
+
+    private IEnumerator WingFlap()
+    {
+        float t_TimeElapsed = 0f;
+        float t_Direction = 1f;
+        while (WingTarget)
+        {
+            float t_Height = Mathf.Lerp(-FlapHeight, FlapHeight, t_TimeElapsed / TimeForCycle);
+            WingTarget.transform.localPosition = new Vector3(WingTarget.transform.localPosition.x, t_Height, WingTarget.transform.localPosition.z);
+            if(t_TimeElapsed >= TimeForCycle)
+            {
+                t_Direction = -1;
+            }
+            if (t_TimeElapsed <= 0)
+            {
+                t_Direction = 1;
+            }
+            t_TimeElapsed += Time.deltaTime * t_Direction;
+            yield return null;
+        }
+
+        yield return null;
+    }
+}
