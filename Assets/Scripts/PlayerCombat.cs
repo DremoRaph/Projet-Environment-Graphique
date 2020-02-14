a7dbcc177daeff81b85cd3cc64dc26b463e4a7b9
TheDude95100 <58417329+TheDude95100@users.noreply.github.com>
2020-02-13 21:54:11 -0500

    Animator et correction de bug

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -3,40 +3,85 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
-{   public float attackDuration = 1f;
-    public float attackRange = 1f;
+{ 
     public BoxCollider playerWeapon;
     public GameObject Pivot;
     public float damage;
     Animator animator;
    public BoxCollider Weapon;
-    bool isAttacking;
+    public int noOfClicks = 0;
+    float lastClickedTime = 0;
+    public float maxComboDelay = 1.2f;
     private void Start()
-    {   
+    {
         animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
         if (Input.GetMouseButtonDown(0))
         {
-            animator.SetTrigger("isAttacking");
-        
+            lastClickedTime = Time.time;
+            noOfClicks++;
+            if (noOfClicks == 1)
+            {
+                animator.SetBool("Attack1", true);
+            }
 
+            noOfClicks = Mathf.Clamp(noOfClicks, 0, 3);
         }
         if (Input.GetMouseButtonDown(1))
         {
+            
             animator.SetTrigger("HighSpin");
         }
         if (Input.GetKeyDown(KeyCode.H))
         {
             animator.SetTrigger("IsCasting");
         }
-     
+        if (Time.time - lastClickedTime > maxComboDelay)
+        {
+            noOfClicks = 0;
+        }
 
-     
+        
+
+
+    }
+    public void return1()
+    {
+        if (noOfClicks >= 2)
+        {
+            animator.SetBool("Attack2", true);
+        }
+        else
+        {
+            animator.SetBool("Attack1", false);
+            noOfClicks = 0;
+        }
+    }
+
+    public void return2()
+    {
+        if (noOfClicks >= 3)
+        {
+            animator.SetBool("Attack3", true);
+        }
+        else
+        {
+            animator.SetBool("Attack2", false);
+            noOfClicks = 0;
+        }
+    }
+
+    public void return3()
+    {
+        animator.SetBool("Attack1", false);
+        animator.SetBool("Attack2", false);
+        animator.SetBool("Attack3", false);
+        noOfClicks = 0;
     }
     private void OnTriggerEnter(Collider col)
     {
