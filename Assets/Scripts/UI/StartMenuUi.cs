ed3461015ace66ef7456e89ed156ae0fa649901d
Thorekt <35566455+Thorekt@users.noreply.github.com>
2020-02-17 13:00:18 -0500

    loading ui

--- Assets/Scripts/UI/StartMenuUi.cs
+++ Assets/Scripts/UI/StartMenuUi.cs
@@ -3,10 +3,19 @@ using UnityEngine.UI;
 
 public class StartMenuUi : MonoBehaviour
 {
+    [SerializeField]
+    Button startButton;
+    [SerializeField]
+    Button optionButton;
+    [SerializeField]
+    Button exitButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startButton.onClick.AddListener(StartButtonClicked);
+        optionButton.onClick.AddListener(OptionButtonClicked);
+        exitButton.onClick.AddListener(ExitButtonClicked);
     }
 
     // Update is called once per frame
@@ -14,4 +23,19 @@ public class StartMenuUi : MonoBehaviour
     {
         
     }
+    void StartButtonClicked()
+    {
+        Debug.Log("StartButtonClicked");
+    }
+
+    void OptionButtonClicked()
+    {
+        Debug.Log("OptionButtonClicked");
+    }
+
+    void ExitButtonClicked()
+    {
+        Debug.Log("ExitButtonClicked");
+    }
+
 }
