b21289c5726bbf50d948eff69e5433e36d421f65
Raphaël Lavoie <rapolysec@hotmail.com>
2020-02-27 13:18:12 -0500

    Remise à niveau tests animation

--- Assets/Scripts/Animations/BirdAnimation.cs
+++ Assets/Scripts/Animations/BirdAnimation.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BirdAnimation : MonoBehaviour
 {
     public GameObject WingTarget;
+    public bool IsFlying = true;
     [Range(0.0f, 5f)]
     public float Velocity = 0f;
     [Range(0.0f, 180.0f)]
@@ -18,6 +19,9 @@ public class BirdAnimation : MonoBehaviour
     private GameObject m_Wing1;
     private GameObject m_Wing2;
     private float m_InitialWingAngle;
+    private Vector3 m_InitialWingTargetPosition;
+    private bool m_IsFlapping = false;
+    private float m_AnimationProgression = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,13 +32,14 @@ public class BirdAnimation : MonoBehaviour
             m_Wing2 = WingTarget.transform.Find("Wing2Target").gameObject;
             m_Wing2.transform.localPosition = new Vector3(0, 0, -WingsSpan);
             m_InitialWingAngle = WingTarget.transform.localPosition.x;
-            StartCoroutine(WingFlap());
-        }
+            m_InitialWingTargetPosition = m_Wing1.transform.parent.localPosition;
+}
     }
 
     // Update is called once per frame
     void Update()
     {
+        InitiateFlyingAnimation();
         transform.Find("Body").localRotation = Quaternion.Euler(new Vector3((-WingsAngle + 90) / 1.5f, 0, 0));
         transform.Find("Wings").localRotation = Quaternion.Euler(new Vector3(-WingsAngle + 90, 0, 0));
         float t_TransformedWingSpan = Mathf.Lerp(WingsSpan, 0.4f, Velocity / 5);
@@ -42,12 +47,33 @@ public class BirdAnimation : MonoBehaviour
         m_Wing2.transform.localPosition = new Vector3(m_Wing2.transform.localPosition.x, m_Wing2.transform.localPosition.y, -t_TransformedWingSpan);
     }
 
-    private IEnumerator WingFlap()
+    public void InitiateFlyingAnimation()
     {
-        float t_TimeElapsed = 0f;
+        if(IsFlying && !m_IsFlapping)
+        {
+            StartCoroutine(WingFlap(m_AnimationProgression));
+            Debug.Log("ANIMATION STARTING");
+        } else if(!IsFlying && m_IsFlapping)
+        {
+            //StopCoroutine(WingFlap());
+            Debug.Log("ANIMATION STOPPED");
+            m_IsFlapping = false;
+        }
+    }
+
+    private IEnumerator WingFlap(float a_TimeRecuperation)
+    {
+        m_IsFlapping = true;
+        Debug.Log(a_TimeRecuperation);
+        float t_TimeElapsed = a_TimeRecuperation;
         float t_Direction = 1f;
         while (WingTarget)
         {
+            if (!m_IsFlapping)
+            {
+                m_AnimationProgression = t_TimeElapsed;
+                yield break;
+            }
             float t_FlapHeight = Mathf.Lerp(MaxFlapHeight, 0, Velocity / 5);
             float t_Height = Mathf.Lerp(-t_FlapHeight, t_FlapHeight, t_TimeElapsed / TimeForCycle);
             WingTarget.transform.localPosition = new Vector3( -WingsDrawBack - Velocity / 10, t_Height, WingTarget.transform.localPosition.z);
---
+++ Assets/Scripts/Animations/DynamicMeshMaker.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicMeshMaker : MonoBehaviour
+{
+    [System.Serializable]
+    public class Triangle{
+        public int PointA, PointB, PointC;
+    }
+    private Mesh m_Mesh;
+    public List<Triangle> Triangles = new List<Triangle>();
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Generate();
+    }
+
+    public void Generate()
+    {
+        GetComponent<MeshFilter>().mesh = m_Mesh = new Mesh();
+        m_Mesh.name = "Procedural Shape";
+        List<Vector3> t_Vertices = new List<Vector3>();
+        foreach (Transform Vertice in transform)
+        {
+            t_Vertices.Add(Vertice.localPosition);
+            //Debug.Log(Vertice.gameObject.name);
+        }
+        m_Mesh.vertices = t_Vertices.ToArray();
+        int[] triangles = new int[6];
+        triangles[0] = 1;
+        triangles[1] = 2;
+        triangles[2] = 3;
+        triangles[3] = 1;
+        triangles[4] = 3;
+        triangles[5] = 2;
+        
+        List<int> t_MeshTriangles = new List<int>();
+        foreach(Triangle t_triangle in Triangles)
+        {
+            t_MeshTriangles.Add(t_triangle.PointA - 1);
+            t_MeshTriangles.Add(t_triangle.PointB - 1);
+            t_MeshTriangles.Add(t_triangle.PointC - 1);
+            t_MeshTriangles.Add(t_triangle.PointA - 1);
+            t_MeshTriangles.Add(t_triangle.PointC - 1);
+            t_MeshTriangles.Add(t_triangle.PointB - 1);
+        }
+        
+        m_Mesh.triangles = t_MeshTriangles.ToArray();
+        //m_Mesh.triangles = triangles;
+        
+
+    }
+}
