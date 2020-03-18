015b482606b20e4ec6056ecb8f41b8b49254221a
Thorekt <35566455+Thorekt@users.noreply.github.com>
2020-03-18 13:41:28 -0400

    refactoring et fix

--- Assets/Scripts/MapGenerator/MapDisplay.cs
+++ Assets/Scripts/MapGenerator/MapDisplay.cs
@@ -18,7 +18,7 @@ public class MapDisplay : MonoBehaviour
 	{
 		meshFilter.sharedMesh = meshData.CreateMesh();
 
-		meshFilter.transform.localScale = Vector3.one * FindObjectOfType<MapGenerator>().meshSettings.meshScale;
+		meshFilter.transform.localScale = Vector3.one * FindObjectOfType<MapPreview>().meshSettings.meshScale;
 	}
 
 }
